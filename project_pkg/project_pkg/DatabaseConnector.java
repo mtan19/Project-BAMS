9fffd943c10793f0b00bfb1cf101149882644143
mtan19 <70813355+mtan19@users.noreply.github.com>
2023-07-06 02:05:40 -0400

    started coding DatabaseConnector.java

--- project_pkg/project_pkg/DatabaseConnector.java
+++ project_pkg/project_pkg/DatabaseConnector.java
@@ -1,5 +1,28 @@
 package project_pkg;
+import java.sql.*;
 
 public class DatabaseConnector {
 	
+	private Connection dbConnection;
+	private Statement dbStatement;
+	
+	//constructor
+	public DatabaseConnector(String url, String username, String password) throws Exception{
+		try{
+			dbConnection = DriverManager.getConnection(url, username, password);
+			dbStatement =  dbConnection.createStatement();
+		}
+		catch(SQLException e) {
+			throw new IllegalArgumentException("Error: Failed to establish connection to with database, invalid parameters.\n" + e.toString());
+		}
+	}
+	
+	private ResultSet runQuery(String query) throws Exception{
+		try {
+			return(dbStatement.executeQuery(query));
+		}
+		catch(SQLException e){
+			throw new IllegalArgumentException("Error: database query failed" + e.toString());
+		}
+	}
 }
